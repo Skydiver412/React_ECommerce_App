36ee70374c056fc5e1e551e71d55a1c33f81e592
Skydiver412 <bkunkel412@yahoo.com>
2021-11-25 15:51:46 -0500

    Updated CSS

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from 'react';
+import { useProductsContext } from '../context/products_context';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Error from './Error';
+import Loading from './Loading';
+import Product from './Product';
 
 const FeaturedProducts = () => {
-  const {products_loading: loading, products_error:error, 
-    featured_products: featured} = useProductsContext();
-  
-  if(loading) {
-    return <Loading />
+  const {
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext();
+
+  if (loading) {
+    return <Loading />;
   }
-  if(error) {
-    return <Error />
+  if (error) {
+    return <Error />;
   }
 
   return (
@@ -24,16 +27,13 @@ const FeaturedProducts = () => {
         <div className='underline'></div>
       </div>
       <div className='section-center featured'>
-        {featured.slice(0,3).map((product) => {
-          return <Product key={product.id} {...product} />
+        {featured.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />;
         })}
-        <Link to='/products' className='btn'>
-          all products
-        </Link>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
@@ -56,6 +56,6 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
-`
+`;
 
-export default FeaturedProducts
+export default FeaturedProducts;
